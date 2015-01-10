998a001d0fc1b2c57095cbc6547a8b693a5d034d
Quildreen Motta <quildreen@gmail.com>
2015-01-10 13:23:37 -0200

    Includes logo and fixes theme options

--- source/conf.py
+++ source/conf.py
@@ -107,7 +107,10 @@ html_theme = 'caffeine'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "version_y": "-45px",
+    "version_x": "175px"
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['caffeine-theme']
@@ -121,7 +124,7 @@ html_theme_path = ['caffeine-theme']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "../logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
